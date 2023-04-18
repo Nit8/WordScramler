293038c658a156e7f4900676a523aaa70f5cc986
Nit8 <47635411+Nit8@users.noreply.github.com>
2023-04-18 14:51:38 +0530

    learning arrays

---
+++ WordScrambler/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordScrambler
+{
+    class ProgramLearn
+    {
+
+        //VALUE TYPES AND REFERENCE TYPES
+        static void Main(string[] args)
+        {
+            int a = 10;
+            ChangeNumber(a);
+            Console.WriteLine(a);
+        }
+        static void ChangeNumber(int a)
+        {
+            a = 90;
+        }
+    }
+}
