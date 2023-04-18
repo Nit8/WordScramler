3650a95da3178a1e2c6e7f394315f7f42ced449f
Nit8 <47635411+Nit8@users.noreply.github.com>
2023-04-18 15:52:18 +0530

    reading and writing File

--- WordScrambler/Program.cs
+++ WordScrambler/Program.cs
@@ -3,24 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
+
+//READ AND WRITE A FILE
 namespace WordScrambler
 {
     class Program
     {
-
-        //readOnly vs Constant
-        public const string myString = "Hey There";
-        //public static readonly string myOtherString = "Hey there again";
-        public readonly string myOtherString = "Hey there again";
-
-        public readonly Person myPerson = new Person("A", "B");
         static void Main(string[] args)
         {
-            Program program = new Program();
-            Console.WriteLine(myString);
-            Console.WriteLine(program.myPerson.FirstName);
-
+            string[] lines = { "This is the first line", "This is a second line", "This is a third line" };
+            File.WriteAllLines("MyFile.txt", lines);
+            string[] fileContent = File.ReadAllLines("./MyFile.txt");
+            Console.WriteLine(fileContent[0]);
         }
     }
 }
